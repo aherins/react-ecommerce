6a18d67be253283c64f3644c4a6a9ef82bc4016f
aherins <herrerainsua.angel@gmail.com>
2026-05-30 18:11:56 +0200

    feat: agregar AuthProvider y CheckoutPage en la estructura de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { StoreProvider } from './context/StoreContext'
+import { AuthProvider } from './context/AuthContext'
 import StoreFront from './pages/StoreFront'
 import ProductDetail from './pages/ProductDetail'
 import CartPage from './pages/CartPage'
+import CheckoutPage from './pages/CheckoutPage'
 import AdminLayout from './pages/admin/AdminLayout'
 import AdminProducts from './pages/admin/AdminProducts'
 import AdminCategories from './pages/admin/AdminCategories'
@@ -12,21 +14,24 @@ import AdminDashboard from './pages/admin/AdminDashboard'
 export default function App() {
   return (
     <StoreProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Tienda */}
-          <Route path="/" element={<StoreFront />} />
-          <Route path="/producto/:id" element={<ProductDetail />} />
-          <Route path="/carrito" element={<CartPage />} />
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* Tienda */}
+            <Route path="/" element={<StoreFront />} />
+            <Route path="/producto/:id" element={<ProductDetail />} />
+            <Route path="/carrito" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
 
-          {/* Admin */}
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="productos" element={<AdminProducts />} />
-            <Route path="categorias" element={<AdminCategories />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+            {/* Admin */}
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route index element={<AdminDashboard />} />
+              <Route path="productos" element={<AdminProducts />} />
+              <Route path="categorias" element={<AdminCategories />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </StoreProvider>
   )
 }
